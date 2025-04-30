ac19b699c912b35f273d69e3897f1616dee03833
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-04-30 03:25:55 +0100

    Update settings.py
    Save

--- BANK/BANK/settings.py
+++ BANK/BANK/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4rd#l1#43)5*ebjwuix9)m+tzvb282c^!s%*0y8j0farxs+u6s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['axisfirstbank.onrender.com']
 
 
 # Application definition
