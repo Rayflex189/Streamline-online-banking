d02031cccc2a0763f7a16b12757665aa0a270b3c
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-04-30 17:39:08 +0100

    Update admin.py
    Save

--- BANK/bank_app/admin.py
+++ BANK/bank_app/admin.py
@@ -9,6 +9,14 @@ class UserProfileAdmin(admin.ModelAdmin):
     list_display = ['user', 'account_number']  # Specify fields to display in the admin list
     search_fields = ['user__username']  #
 
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ['user', 'amount', 'balance_after', 'timestamp', 'description']  # Specify fields to display in the admin list
+    search_fields = ['user__username', 'description']  # Search by user and description
+    list_filter = ['timestamp', 'user']  # Add filters for timestamp and user
+    ordering = ['-timestamp']
+    
+
 class YourModelAdmin(admin.ModelAdmin):
     list_display = ('image_display')
 
