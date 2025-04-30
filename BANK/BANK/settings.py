b7d121d5361041e3a500a09284d370570a9ae566
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-04-30 17:14:22 +0100

    Update settings.py
    Save

--- BANK/BANK/settings.py
+++ BANK/BANK/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+SUPERUSER_NAME = 'Admin'
+SUPERUSER_PASSWORD = 'Me12sleep'
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
