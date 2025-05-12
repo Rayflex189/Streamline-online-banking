fd83979a2ab4c21292df9be5f5881e5d1b5daf4d
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-05-11 22:47:37 -1100

    Update build.sh
    Save

--- BANK/build.sh
+++ BANK/build.sh
@@ -5,15 +5,18 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
-# Run database migrations (core apps first)
-python manage.py migrate contenttypes --no-input
-python manage.py migrate auth --no-input
-python manage.py migrate sessions --no-input
-python manage.py migrate admin --no-input
-
-# Your app migrations
+# Run database migrations
 python manage.py makemigrations
-python manage.py migrate --no-input
+python manage.py migrate
+
+# Create default superuser
+echo "from django.contrib.auth import get_user_model; \
+from django.conf import settings; \
+User = get_user_model(); \
+username = settings.SUPERUSER_NAME; \
+password = settings.SUPERUSER_PASSWORD; \
+User.objects.filter(username=username).exists() or User.objects.create_superuser(username=username, password=password)" \
+| python manage.py shell
 
 # Collect static files
 python manage.py collectstatic --no-input
