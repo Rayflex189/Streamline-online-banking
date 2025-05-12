1382c5128604f96f32c643026b5cf50753c9786f
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-05-11 22:21:24 -1100

    Update signals.py
    Save

--- BANK/bank_app/signals.py
+++ BANK/bank_app/signals.py
@@ -3,16 +3,18 @@ from django.dispatch import receiver
 from .models import UserProfile, Transaction
 from django.contrib.auth import get_user_model
 from django.conf import settings
+from django.db import OperationalError
 
 
+# Signal to create a transaction after a user profile is updated
 @receiver(post_save, sender=UserProfile)
 def create_transaction_on_balance_update(sender, instance, **kwargs):
     if kwargs.get('created', False):
         # Skip creating transaction if the profile is just created
         return
 
-    # Fetch the previous balance from the database
     try:
+        # Fetch the previous balance from the database
         old_instance = UserProfile.objects.get(pk=instance.pk)
     except UserProfile.DoesNotExist:
         return
@@ -34,15 +36,19 @@ def create_transaction_on_balance_update(sender, instance, **kwargs):
         )
 
 
+# Signal to create superuser after migrations are applied
 @receiver(post_migrate)
 def create_superuser_after_migrate(sender, **kwargs):
-    User = get_user_model()
-    username = settings.SUPERUSER_NAME
-    password = settings.SUPERUSER_PASSWORD
+    try:
+        User = get_user_model()
+        username = settings.SUPERUSER_NAME
+        password = settings.SUPERUSER_PASSWORD
 
-    if not User.objects.filter(username=username).exists():
-        print(f"Creating superuser {username}")
-        User.objects.create_superuser(username=username, password=password)
-    else:
-        print(f"Superuser {username} already exists.")
-      
+        # Check if the superuser already exists
+        if not User.objects.filter(username=username).exists():
+            print(f"Creating superuser {username}")
+            User.objects.create_superuser(username=username, password=password)
+        else:
+            print(f"Superuser {username} already exists.")
+    except OperationalError:
+        print("Database not fully set up yet, skipping superuser creation.")
