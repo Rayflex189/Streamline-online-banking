fd75aecf6e9f7cde08fe31066b524b2ce1f8c21f
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2025-05-11 22:26:59 -1100

    Update build.sh
    Save

--- BANK/build.sh
+++ BANK/build.sh
@@ -5,9 +5,15 @@ set -o errexit
 # Install dependencies
 pip install -r requirements.txt
 
-# Run database migrations
+# Run database migrations (core apps first)
+python manage.py migrate contenttypes --no-input
+python manage.py migrate auth --no-input
+python manage.py migrate sessions --no-input
+python manage.py migrate admin --no-input
+
+# Your app migrations
 python manage.py makemigrations
-python manage.py migrate
+python manage.py migrate --no-input
 
 # Collect static files
 python manage.py collectstatic --no-input
